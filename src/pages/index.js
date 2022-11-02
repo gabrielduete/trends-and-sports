e3a2fcbf1361ca759bf40883f9201decdec00a7f
Gabriel <gabrielmonteiroduete@gmail.com>
2022-11-02 19:47:59 -0300

    chore: organize initial structure

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+function App() {
+  return <section className="App" />
+}
+
+export default App
---
+++ src/pages/index.test.js
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react"
+import App from "."
+
+test("<App />", () => {
+  render(<App />)
+  it.todo("should render app correctly")
+})
